8f902f2c11feed7f960761e8cffceb04bde2e8d6
Yoanna Popova <yoanna.dp@gmail.com>
2025-05-02 10:02:48 +0300

    Add utils file.

---
+++ 08-SPA-exercise/01-forum/utils.js
@@ -0,0 +1,16 @@
+// Here are template functions which we can use in anither files.
+
+export function createElements(type, content, parent, attributes) {
+  let element = document.createElement(type);
+  element.textContent = content;
+
+  if (parent) {
+    parent.appendChild(element);
+  }
+
+  for (const attribute of Object.keys(attributes)) {
+    element.setAttribute(attribute, attributes[attribute]);
+  }
+  
+  return element;
+}
