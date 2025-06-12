bbe23118cb85915c8d34d9335833d84cacafc10f
Yoanna Popova <yoanna.dp@gmail.com>
2025-06-12 11:36:15 +0300

    Add api business logic.

---
+++ 13-routing-lab/demo-page-js/src/data/api.js
@@ -0,0 +1,38 @@
+const host = 'http://localhost:3030';
+
+async function request(method, url, data) {
+    const options = {
+        method,
+        headers: {}
+    }
+
+    if (data !== undefined) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    try {
+        const response = await fetch(host + url, options);
+
+        if (response.status == 204) { // 204 means the response is ok, but we don't have content.
+            return response;
+        }
+
+        const data = await response.json();
+
+        if (response.ok == false) {
+            throw new Error(data.message);
+        }
+
+        return data;
+
+    } catch (err) {
+        alert(err.message);
+        throw err;
+    }
+}
+
+export const get = request.bind(null, 'GET');
+export const post = request.bind(null, 'POST');
+export const put = request.bind(null, 'PUT');
+export const del = request.bind(null, 'DELETE');
