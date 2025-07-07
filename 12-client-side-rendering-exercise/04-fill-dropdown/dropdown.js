68fbe1f27025d7bf8ea57355e03b12d4ae78ebad
Yoanna Popova <yoanna.dp@gmail.com>
2025-07-07 15:47:41 +0300

    Add the file with business logic for Fill Dropdown project.

---
+++ 12-client-side-rendering-exercise/04-fill-dropdown/dropdown.js
@@ -0,0 +1,46 @@
+import { html, render } from './node_modules/lit-html/lit-html.js';
+
+const url = 'http://localhost:3030/jsonstore/advanced/dropdown';
+
+const root = document.getElementById('menu');
+
+const form = document.querySelector('form');
+form.addEventListener('submit', onSubmit);
+
+onLoadContent();
+
+// This function is for the 'GET' request.
+async function onLoadContent() {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const result = Object.values(data).map(x => createOptionTemplate(x));
+
+    render(result, root);
+}
+
+// This function creates HTML template.
+function createOptionTemplate(option) {
+    return html`
+        <option value=${option._id}>${option.text}</option>
+    `
+}
+
+function onSubmit(e) {
+    e.preventDefault();
+    const value = document.getElementById('itemText').value;
+    value && addItem(value);
+}
+
+async function addItem(data) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text: data })
+    })
+
+    form.reset();
+    onLoadContent();
+}
