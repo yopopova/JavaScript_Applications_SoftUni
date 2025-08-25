384ae6e5cceca1bc875b1394b5ef488565a358c7
Yoanna Popova <yoanna.dp@gmail.com>
2025-08-25 16:34:58 +0300

    e for Templating Lit HTML demo.

---
+++ 11-client-side-rendering-lab/demo-templating-lit-html-jan-2023/src/data.js
@@ -0,0 +1,20 @@
+// This file is like server.
+
+export const data = [
+    {
+        name:'Peter',
+        phone: '+1-555-3897'
+    },
+    {
+        name:'John',
+        phone: '+1-555-8871'
+    },
+    {
+        name:'Mary',
+        phone: '+1-555-9495'
+    }
+]
+
+export const products = [
+
+]
