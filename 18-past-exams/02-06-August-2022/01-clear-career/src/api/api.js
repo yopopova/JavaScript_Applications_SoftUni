039e1fdd9c4d9ef7a0adb4d5f2951c3954e214ba
Yoanna Popova <yoanna.dp@gmail.com>
2025-09-24 23:44:35 +0300

    Add the file with APIs for Clear Career app.

---
+++ 18-past-exams/02-06-August-2022/01-clear-career/src/api/api.js
@@ -0,0 +1,53 @@
+import { getUserData } from "../util.js";
+
+const host = 'http://localhost:3030';
+
+async function request(method, url, data) {
+    const options = {
+        method,
+        headers: {}
+    }
+
+    if (data !== undefined) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    const user = getUserData();
+
+    if (user) {
+        options.headers['X-Authorization'] = user.accessToken;
+    }
+
+    try {
+        const response = await fetch(host + url, options);
+
+        if (response.status == 204) { // The request is OK, but we don't have DATA.
+            return response;
+        }
+
+        const result = await response.json();
+
+        if (response.ok == false) { // Here we catch also the 403 error.
+            throw new Error(result.message);
+        }
+
+        return result;
+
+    } catch (err) {
+        alert(err.message);
+        throw err;
+    }
+}
+
+const get = request.bind(null, 'GET');
+const post = request.bind(null, 'POST');
+const put = request.bind(null, 'PUT');
+const del = request.bind(null, 'DELETE');
+
+export {
+    get,
+    post,
+    put,
+    del,
+}
