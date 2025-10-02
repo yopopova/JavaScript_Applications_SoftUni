b6598b8afb4577c3fb99aa47531bdf1c0ea2288b
Yoanna Popova <yoanna.dp@gmail.com>
2025-10-02 11:00:52 +0300

    Add edit view for Fruitipedia app.

---
+++ 18-past-exams/03-01-April-2023/01-fruitipedia/src/views/edit.js
@@ -0,0 +1,67 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { getById, editFruit} from "../api/data.js";
+import { createSubmitHandler } from "../util.js";
+
+const editTemplate = (fruit, onEdit) => html`
+    <section id="edit">
+          <div class="form">
+            <h2>Edit Fruit</h2>
+            <form @submit=${onEdit} class="edit-form">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Fruit Name"
+                .value=${fruit.name}
+              />
+              <input
+                type="text"
+                name="imageUrl"
+                id="Fruit-image"
+                placeholder="Fruit Image URL"
+                .value=${fruit.imageUrl}
+              />
+              <textarea
+                id="fruit-description"
+                name="description"
+                placeholder="Description"
+                rows="10"
+                cols="50"
+                .value=${fruit.description}
+              ></textarea>
+              <textarea
+                id="fruit-nutrition"
+                name="nutrition"
+                placeholder="Nutrition"
+                rows="10"
+                cols="50"
+                .value=${fruit.nutrition}
+              ></textarea>
+              <button type="submit">post</button>
+            </form>
+          </div>
+    </section>`
+
+
+export async function showEdit(ctx) {
+    const id = ctx.params.id;
+    const fruit = await getById(id);
+
+    ctx.render(editTemplate(fruit, createSubmitHandler(onEdit)));
+
+    async function onEdit({ name, imageUrl, description, nutrition }, form) {
+        if (name == '' || imageUrl == '' || description == '' || nutrition == '') {
+            return alert('All fields are required!');
+        }
+
+        await editFruit(id, {
+            name,
+            imageUrl,
+            description,
+            nutrition
+        })
+
+        form.reset();
+        ctx.page.redirect('/catalog/' + id);
+    }
+}
